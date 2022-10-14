105a07ac21af69b44df8cdd275a02651670499b1
Yes-2 <106625338+Yes-2@users.noreply.github.com>
2022-10-14 11:16:15 -0400

    Add files via upload

---
+++ First script.lua
@@ -0,0 +1,35 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Swag Scripts", "DarkTheme")
+
+---MAIN
+local Main = Window:NewTab("Main")
+local MainSection = Main:NewSection("Swag Scripts")
+
+
+MainSection:NewToggle("Super Human", "jump and go speed", function(state)
+    if state then
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 120
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = 120
+    else
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+    end
+end)
+
+
+--localplayer
+local Player = Window:NewTab("Player")
+local PlayerSection = Player:NewSection("Swag Scripts")
+
+PlayerSection:NewSlider("Walkspeed", "Speed slider", 500, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+PlayerSection:NewSlider("JumpPower", "Rabbit", 350, 50, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
+
+PlayerSection:NewButton("Reset WS/JP", "Resets to all defaults", function()
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+end)
